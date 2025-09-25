e932581c5cf9c539ab7f6cda92d7b1acf7ca5d04
peerapong_p730 <panwongsa_p@su.ac.th>
2025-09-25 03:18:21 +0000

    bookstore101-routing

---
+++ bookstore-app/src/pages/CategoryPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const CategoryPage = () => {
+  return (
+    <div>
+      <h1>This Page CategoryPage</h1>
+      <p>Go BACK!!.</p>
+      <p>I am not ready yet</p>
+    </div>
+  );
+};
+
+export default CategoryPage;
---
+++ bookstore-app/src/pages/ContactPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>This Page ContactPage</h1>
+      <p>Go BACK!!.</p>
+      <p>I am not ready yet</p>
+    </div>
+  );
+};
+
+export default ContactPage;
